2ac62bf843b2106f3617de1cabcf297517f72bee
RanaFaiz <rfaiz6537@gmail.com>
2024-11-07 18:05:04 +0500

    this is all seting

---
+++ react/src/layout/Dashboard/Drawer/DrawerContent/NavCard.jsx
@@ -0,0 +1,24 @@
+// material-ui
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import CardMedia from '@mui/material/CardMedia';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+// project-imports
+import MainCard from 'components/MainCard';
+import AnimateButton from 'components/@extended/AnimateButton';
+
+// assets
+import { Logout } from 'iconsax-react';
+import avatar from 'assets/images/analytics/welcome-banner.png';
+
+// ==============================|| DRAWER CONTENT - NAV CARD ||============================== //
+
+export default function NavCard() {
+  return (
+    <MainCard sx={{ bgcolor: 'secondary.lighter', m: 3 }}>
+      
+    </MainCard>
+  );
+}
