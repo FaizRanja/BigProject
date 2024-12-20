f9a901d44fe7c9db8d37b0582e6d3770775e2893
RanaFaiz <rfaiz6537@gmail.com>
2024-12-20 17:52:10 +0500

    sdaf

--- server/src/routes/chatbot.routes.js
+++ server/src/routes/chatbot.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { UserChatSignup } = require('../controller/Chatboat');
+const { UserChatSignup, checkUserByIp } = require('../controller/Chatboat');
 
 const router=express.Router()
 
 router.route('/chat').post(UserChatSignup)
-
+// router.route('/checkUserByIp').get(checkUserByIp)
 
 module.exports = router;
+
+
